d01c8ad3f270a8f4ec23ac08a0cb37930f327eea
Timothy <timmyac940@gmail.com>
2025-05-19 16:24:03 +1000

    added to the student model

--- models/student.py
+++ models/student.py
@@ -1,4 +1,8 @@
 class Student:
     
-    def __init__(self):
-        pass
+    def __init__(self, id, name, email, subjects):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.subjects = subjects
+
