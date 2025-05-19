077196bd062c2b038d456145e434bd473a88b23e
Timothy <timmyac940@gmail.com>
2025-05-19 16:29:07 +1000

    added to the student system controller

--- controllers/studentSystemController.py
+++ controllers/studentSystemController.py
@@ -2,17 +2,26 @@ from models.student import Student
 from models.subject import Subject
 
 class StudentSystemController:
+
+    def login(self):
+        pass
+
+    def register(self):
+        pass
     
     def system(self):
         userInput = input("\033[96mStudent System (l/r/x): \033[0m")
         while(userInput != "x"):
             match userInput:
                 case "l":
-                    pass
+                    self.login()
+
                 case "r":
-                    pass
+                    self.register()
+
                 case "x":
                     pass
+                
                 case _:
                     pass
             userInput = input("\033[96mStudent System (l/r/x): \033[0m")
