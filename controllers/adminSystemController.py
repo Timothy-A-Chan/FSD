e7d804d9292c45c19beafee602588b99c9e73e06
Timothy <timmyac940@gmail.com>
2025-05-20 17:41:16 +1000

    fixed grouping

--- controllers/adminSystemController.py
+++ controllers/adminSystemController.py
@@ -1,10 +1,46 @@
+from models.database import Database as db
+
 class AdminSystemController:
 
     def clearDatabase(self):
         pass
 
     def groupStudents(self):
-        pass
+        print("Grade Grouping")
+        students = db.read()
+
+        grade_order = {
+        "HD": 4,
+        "D": 3,
+        "C": 2,
+        "P": 1,
+        "Z": 0,
+        }
+
+        for student in students:
+            total = sum(subject.mark for subject in student.subjects)
+            average = total / len(student.subjects)
+
+                # Assign grade based on average
+            if average >= 85:
+                student.grade = "HD"
+            elif 75 <= average < 85:
+                student.grade = "D"
+            elif 65 <= average < 75:
+                student.grade = "C"
+            elif 50 <= average < 65:
+                student.grade = "P"
+            else:
+                student.grade = "Z"
+
+            student.mark = average
+
+            students.sort(key=lambda s: grade_order.get(s.mark), reverse=True)
+
+            db.write(students)
+
+            for student in students:
+                print(f"{student.grade} --> [{student.name} :: {str(student.id)} --> Grade: {student.mark:.2f}]")
 
     def partitionStudents(self):
         pass
@@ -13,7 +49,10 @@ class AdminSystemController:
         pass
 
     def show(self):
-        pass
+        print("Student List")
+        students = db.read()
+        for student in students:
+            print(student.name + " :: " + str(student.id) + " --> Email: " + student.email)
 
     def system(self):
         userInput = input("\033[96mAdmin System (c/g/p/r/s/x): \033[0m")
--- models/student.py
+++ models/student.py
@@ -1,9 +1,11 @@
 class Student:
     
-    def __init__(self, id, name, email, password, subjects = None):
+    def __init__(self, id, name, email, password, subjects = None, grade = None, mark = None):
         self.id = id
         self.name = name
         self.email = email
         self.password = password
         self.subjects = subjects if subjects is not None else []
+        self.grade = grade
+        self.mark = mark
 
